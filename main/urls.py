b0736fa85324b0a972c795e24f8ef4582b9a8642
Nadekoooo <christianyudistira09@gmail.com>
2024-10-22 20:14:30 +0700

    navbar adjusting

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_att, name='show_att'),
+    # path('login/', user_login, name = "user_login")
     # path('search/', search_instance, name = 'search_instance')
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 
+# @login_required
 def show_att(request):
     
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
     }
 
     return render(request, "att.html", context)
+
+
+# def user_login(request):
+#     form = AuthenticationForm()
+    
+#     if request.method == 'POST':
+#         form = AuthenticationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             response = HttpResponseRedirect(reverse("main:show_att"))
+#             response.set_cookie('last_login', str(datetime.datetime.now()))
+#             return response
+#         else:
+#             messages.error(request, "Invalid username or password. Please try again.") 
+
+#     context = {'form': form}
+#     return render(request, 'login.html', context)
+
 
 # def search_instance(request):
 #     return render(request, "search_instance.html", )
