d270d56b773c73af401c1577cccd262506622078
william-saputra <williammatthews.id@gmial.com>
2024-12-09 15:39:22 +0700

    add user backend

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from user.views import *
+from . import views
 
 app_name = 'user'
 
@@ -11,4 +12,15 @@ urlpatterns = [
     path('profile/change-password/', change_password, name='change_password'),
     path('toggle_resto_fav/', toggle_resto_fav, name='toggle_resto_fav'),
     path('toggle_food_fav/', toggle_food_fav, name='toggle_food_fav'),
+    path('get_client_data/', get_client_data, name='get_client_data'),
+    
+    path('change-description-flutter/', change_description_flutter, name='change_description_flutter'),
+    path('change-username-flutter/', change_username_flutter, name='change_username_flutter'),
+    path('change-password-flutter/', change_password_flutter, name='change-password-flutter'),
+    path('upload-picture-flutter/', upload_profile_picture_flutter, name='upload_profile_picture_flutter'),
+    path('get-all-restaurants-flutter/', get_all_restaurants_flutter, name='get_all_restaurants_flutter'),
+    path('get-all-foods-flutter/', get_all_foods_flutter, name='get_all_foods_flutter'),
+    path('update-fav-restaurants-flutter/', update_favorite_restaurants_flutter, name='update_favorite_restaurants_flutter'),
+    path('update-fav-foods-flutter/', update_favorite_foods_flutter, name='update_favorite_foods_flutter'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -16,10 +16,13 @@ from django.views.decorators.http import require_POST
 import json
 from django.contrib.auth.decorators import login_required
 
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Client
 
-
-
-
+import base64
+from django.core.files.base import ContentFile
 
 def profile_view(request):
     # try:
@@ -74,6 +77,7 @@ def upload_profile_picture(request):
 
     return redirect('user:profile')
 
+@login_required
 def change_name(request):
     if request.method == 'POST':
         new_name = request.POST.get('new_value')  
@@ -84,20 +88,22 @@ def change_name(request):
             return JsonResponse({'message': 'Username updated successfully!'})
         else:
             return JsonResponse({'error': 'Username cannot be empty.'}, status=400)
-
+@login_required
 @csrf_exempt  
 def change_description(request):
-    if request.method == 'POST':
-        new_description = request.POST.get('new_value') 
+    if request.method == "POST":
+        new_description = request.POST.get('new_value')
+        if not new_description:
+            return JsonResponse({'status': 'error', 'message': 'Description is required.'}, status=400)
 
-        client, created = Client.objects.get_or_create(user=request.user)
-
-        if new_description:
+        try:
+            client = request.user.client  
             client.description = new_description
             client.save()
-            return JsonResponse({'message': 'Description updated successfully!'})
-        else:
-            return JsonResponse({'error': 'Description cannot be empty.'}, status=400)
+            return JsonResponse({'status': 'success'}, status=200)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+
 
 @csrf_exempt  
 @login_required
@@ -164,3 +170,248 @@ def toggle_food_fav(request):
             return JsonResponse({'success': False, 'message': str(e)})
 
     return JsonResponse({'success': False, 'message': 'Invalid request'})
+
+
+@login_required
+def get_client_data(request):
+    try:
+        client = Client.objects.get(user=request.user)
+
+        client_data = {
+            'username': request.user.username,
+            'profile_picture': client.profile_picture.url if client.profile_picture else None,
+            'description': client.description,
+            'favorite_restaurants': list(client.favorite_restaurants.values('id', 'name', 'location')),
+            'favorite_foods': list(client.favorite_foods.values('id', 'name', 'category', 'price')),
+        }
+
+        print(client_data)
+        print("sukses")
+        return JsonResponse({'success': True, 'data': client_data})
+        
+
+    except Client.DoesNotExist:
+        return JsonResponse({'success': False, 'message': 'Client profile not found'}, status=404)
+
+@login_required
+@csrf_exempt 
+@require_POST
+def change_description_flutter(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            new_description = data.get('new_value', '').strip()
+
+            if not new_description:
+                return JsonResponse({'status': 'error', 'message': 'Description is required.'}, status=400)
+
+            client = Client.objects.get(user=request.user)
+            client.description = new_description
+            client.save()
+
+            updated_data = {
+                'username': request.user.username,
+                'description': client.description,
+            }
+
+            return JsonResponse({
+                'status': 'success',
+                'message': 'Description updated successfully!',
+                'data': updated_data
+            }, status=200)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid JSON format.'}, status=400)
+        except Client.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Client profile not found.'}, status=404)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method.'}, status=405)
+
+@login_required
+@csrf_exempt
+@require_POST
+def change_password_flutter(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            current_password = data.get('current_password', '').strip()
+            new_password = data.get('new_password', '').strip()
+
+            if not current_password or not new_password:
+                return JsonResponse(
+                    {'status': 'error', 'message': 'Both current and new passwords are required.'},
+                    status=400
+                )
+
+            user = request.user  
+            if not user.check_password(current_password):
+                return JsonResponse(
+                    {'status': 'error', 'message': 'Current password is incorrect.'},
+                    status=400
+                )
+            user.set_password(new_password)
+            user.save()
+            updated_data = {
+                'username': user.username,
+            }
+            return JsonResponse(
+                {
+                    'status': 'success',
+                    'message': 'Password changed successfully!',
+                    'data': updated_data
+                },
+                status=200
+            )
+        except json.JSONDecodeError:
+            return JsonResponse(
+                {'status': 'error', 'message': 'Invalid JSON format.'},
+                status=400
+            )
+        except Exception as e:
+            return JsonResponse(
+                {'status': 'error', 'message': str(e)},
+                status=500
+            )
+    return JsonResponse(
+        {'status': 'error', 'message': 'Invalid request method.'},
+        status=405
+    )
+
+
+@login_required
+@csrf_exempt 
+@require_POST
+def change_username_flutter(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            new_username = data.get('new_value', '').strip()
+
+            if not new_username:
+                return JsonResponse({'status': 'error', 'message': 'Description is required.'}, status=400)
+
+            user = request.user
+            user.username = new_username
+            user.save()
+
+            updated_data = {
+                'username': user.username,
+                'description': user.client.description,
+            }
+
+            return JsonResponse({
+                'status': 'success',
+                'message': 'Description updated successfully!',
+                'data': updated_data
+            }, status=200)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid JSON format.'}, status=400)
+        except Client.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Client profile not found.'}, status=404)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method.'}, status=405)
+
+
+@login_required
+@csrf_exempt
+@require_POST
+def upload_profile_picture_flutter(request):
+    try:
+        data = json.loads(request.body)
+        profile_picture_base64 = data.get('profile_picture', '')
+        
+        if not profile_picture_base64:
+            return JsonResponse({'status': 'error', 'message': 'No profile picture provided.'}, status=400)
+        
+        format, imgstr = profile_picture_base64.split(';base64,') 
+        ext = format.split('/')[-1] 
+        img_data = ContentFile(base64.b64decode(imgstr), name=f'profile_pic.{ext}')
+        
+        client = Client.objects.get(user=request.user)
+        client.profile_picture = img_data
+        client.save()
+        
+        return JsonResponse({
+            'status': 'success',
+            'message': 'Profile picture updated successfully!',
+            'data': {
+                'profile_picture': client.profile_picture.url if client.profile_picture else None
+            }
+        }, status=200)
+    except Client.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Client profile not found.'}, status=404)
+    except (ValueError, IndexError):
+        return JsonResponse({'status': 'error', 'message': 'Invalid image format.'}, status=400)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+    
+
+@login_required
+def get_all_restaurants_flutter(request):
+    try:
+        restaurants = Restaurant.objects.all().values('id', 'name', 'location')
+        restaurants_list = list(restaurants)
+        return JsonResponse({'status': 'success', 'data': restaurants_list}, status=200)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+
+@login_required
+def get_all_foods_flutter(request):
+    try:
+        foods = Food.objects.all().values('id', 'name', 'category', 'price')
+        foods_list = list(foods)
+        return JsonResponse({'status': 'success', 'data': foods_list}, status=200)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+
+@login_required
+@csrf_exempt
+@require_POST
+def update_favorite_restaurants_flutter(request):
+    try:
+        data = json.loads(request.body)
+        selected_ids = data.get('favorite_restaurants', [])
+        if not isinstance(selected_ids, list):
+            return JsonResponse({'status': 'error', 'message': 'favorite_restaurants must be a list.'}, status=400)
+        
+        client = Client.objects.get(user=request.user)
+        valid_restaurants = Restaurant.objects.filter(id__in=selected_ids)
+        
+        client.favorite_restaurants.set(valid_restaurants)
+        
+        print(f"Selected IDs: {valid_restaurants}")
+
+        return JsonResponse({'status': 'success', 'message': 'Favorite restaurants updated successfully!'}, status=200)
+    except Client.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Client profile not found.'}, status=404)
+    except json.JSONDecodeError:
+        return JsonResponse({'status': 'error', 'message': 'Invalid JSON format.'}, status=400)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+
+@login_required
+@csrf_exempt
+@require_POST
+def update_favorite_foods_flutter(request):
+    try:
+        data = json.loads(request.body)
+        selected_ids = data.get('favorite_foods', [])
+        if not isinstance(selected_ids, list):
+            return JsonResponse({'status': 'error', 'message': 'favorite_foods must be a list.'}, status=400)
+        
+        client = Client.objects.get(user=request.user)
+        valid_foods = Food.objects.filter(id__in=selected_ids)
+        client.favorite_foods.set(valid_foods)
+        
+        return JsonResponse({'status': 'success', 'message': 'Favorite foods updated successfully!'}, status=200)
+    except Client.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Client profile not found.'}, status=404)
+    except json.JSONDecodeError:
+        return JsonResponse({'status': 'error', 'message': 'Invalid JSON format.'}, status=400)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
